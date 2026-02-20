772700c648c6b82ba2b561490e885fd4387a463d
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2026-02-21 00:48:11 +0700

    Update new backend code

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const connectionDB = async () => {
         const url = process.env.DATABASE_URL || "mongodb://localhost:27017/spa-project";
         
         await mongoose.connect(url);
-        console.log(`✅ MongoDB Connected to ${url.includes('mongodb+srv') ? 'Cloud' : 'Local'}`);
+        console.log(`MongoDB Connected to ${url.includes('mongodb+srv') ? 'Cloud' : 'Local'}`);
     } catch (error) {
-        console.error('❌ Connection error:', error);
+        console.error('Connection error:', error);
         process.exit(1);
     }
 }
